d442af181a0f8bdf21826b03a5df49de9e74b684
Arnd-Kisho Langenberg <aklangenberg@gmail.com>
2022-04-19 02:04:11 +0200

    added validation to models Review and Restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+
+  CATEGORIES = %w[chinese japanese italian french belgian].freeze
+  has_many :reviews, dependent: :destroy
+  validates :name, length: { minimum: 1 }
+  validates :address, length: { minimum: 1 }
+  validates :category, length: { minimum: 1 }, inclusion: { in: CATEGORIES }
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :content, length: { minimum: 1 }
+  validates :rating, length: { minimum: 1 }, numericality: true, inclusion: { in: (0..5) }
 end
